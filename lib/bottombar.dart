b6e2013a28fe8522fefc976519e4f05ec1c34969
ozkokelii <omerozkokeli23@gmail.com>
2020-12-04 04:40:36 +0300

    deneme2

---
+++ lib/bottombar.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BottomBar extends StatefulWidget {
+  @override
+  _BottomBarState createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
